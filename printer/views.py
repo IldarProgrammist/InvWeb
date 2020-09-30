e3a7e064342f91aa3a528f66ffe6b1d4e7e3a325
Администратор <ProgrammerIldarik@gmail.com>
2020-09-30 23:04:14 +0300

    добавлена детализация для модели Printer

--- printer/views.py
+++ printer/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from printer.models import *
 from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from printer.models import *
+
 
 class PrinterInfoView(ListView):
     model = PtinterCatrige
@@ -11,22 +12,20 @@ class PrinterInfoView(ListView):
     template_name = 'printer/printer_info.html'
 
 
-
-
-
 def printerListView(request):
-
     searchQwery = request.GET.get('search', '')
     if searchQwery:
         printer = Printer.objects.filter(Q(serialNumber__contains=searchQwery))
     else:
         printer = Printer.objects.all()
 
-    return render(request,'printer/printerList.html', context={'printer_': printer})
-
-
+    return render(request, 'printer/printerList.html', context={'printer_': printer})
 
 
 class PrinterDetailView(DetailView):
-    model = Printer
     template_name = 'printer/listDetail.html'
+
+
+    def get_object(self):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Printer, id=id_)
