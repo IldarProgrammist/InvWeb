09e853dced130b1425ac60dbcd4e2147dd2b1358
Администратор <ProgrammerIldarik@gmail.com>
2020-09-30 23:08:27 +0300

    изменен URL printer Detile детализация для модели Printer

--- printer/urls.py
+++ printer/urls.py
@@ -4,6 +4,6 @@ from printer.views import *
 urlpatterns = [
     path('',  PrinterInfoView.as_view(), name='printer'),
     path('list/', printerListView, name='printerList'),
-    path('list/detile/<int:id>/', PrinterDetailView.as_view(), name = 'printer-detile')
+    path('detile/<int:id>/', PrinterDetailView.as_view(), name = 'printer-detile')
 ]
 
